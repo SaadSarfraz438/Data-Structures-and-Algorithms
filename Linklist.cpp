a9d6bcdcc80572b1307089ba4ff2618b8909f73e
Saad Sarfraz <saadsarfraz438@gmail.com>
2026-05-04 17:45:17 +0500

    day4:function delete implemented

--- Linklist.cpp
+++ Linklist.cpp
@@ -45,16 +45,50 @@ void insertAtposition(int data,int pos){
         return;
     }
    
-    Node *temp = new Node{data,current->next};
-    current->next = temp;
+    Node *temp=new Node{data,current->next};
+    current->next=temp;
 }
 void deleteFromStart(){
-    if(head == nullptr){
+    if(head==nullptr){
         cout<<"List is empty\n";
         return;
     }
-    Node *temp =head;
+    Node*temp=head;
     head = head->next;
     delete temp;
 }
+void deleteFromEnd(){
+    if(head ==nullptr){
+        cout<<"List is empty\n";
+        return;
+    }
+    Node*current = head;
+    while(current->next->next != nullptr){
+        current =current->next;
+    }
+    delete current->next;
+    current->next =nullptr;
+}
+void deleteAtposition(int pos){
+    if(head = nullptr){
+        cout<<"List is empty\n";
+        return;
+    }
+    if (pos ==1){
+        deleteFromStart();
+        return;
+    }
+    Node * current =head;
+    for(int i=1; i<pos && current != nullptr; i++){
+        current =current->next;
+    }
+    if(current == nullptr || current->next == nullptr){
+        cout<<"invalid poistion";
+        return;
+    }
+    Node *temp = current->next;
+    current->next = temp->next;
+    delete temp;
+}
+
 }
